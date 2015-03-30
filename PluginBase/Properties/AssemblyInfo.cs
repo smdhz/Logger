f5d9c7d2a309978d3ae899db7a3b9c981b00516e
smdhz <m.smdhz@gmail.com>
2015-03-30 08:55:20 +0800

    修正版本信息

--- PluginBase/Properties/AssemblyInfo.cs
+++ PluginBase/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("PluginBase")]
+[assembly: AssemblyTitle("Logger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PluginBase")]
+[assembly: AssemblyCompany("Monkeysoft")]
+[assembly: AssemblyProduct("Logger")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
