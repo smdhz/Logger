27e2f6509d21efc3c12407c1d50f00cbff47ccb1
许金 <smdhz@msn.com>
2016-11-18 16:30:05 +0800

    预处理

--- PluginBase/Counter.cs
+++ PluginBase/Counter.cs
@@ -17,8 +17,6 @@ namespace Logger
     {
         public Counter(KanColleProxy proxy)
         {
-            IsOnline = db.DatabaseExists();
-
             proxy.api_req_sortie_battleresult.TryParse<kcsapi_battleresult>().Subscribe(x => Battle(x.Data));
             proxy.api_req_combined_battle_battleresult.TryParse<kcsapi_battleresult>().Subscribe(x => Battle(x.Data));
             proxy.api_port.TryParse<kcsapi_port>().Subscribe(x => OntheWay = false);
@@ -26,10 +24,8 @@ namespace Logger
 
         private bool OntheWay = false;
         private Guid FightID = Guid.Empty;
-        private LogDataContext db = new LogDataContext();
-
-        public bool IsOnline { get; private set; }
-        private ShipLog record;
+        
+        //private ShipLog record;
 
         private void Battle(kcsapi_battleresult data)
         {
@@ -40,34 +36,17 @@ namespace Logger
             }
 
             // 准备数据
-            record = new ShipLog()
-            {
-                Time = DateTime.Now,
-                Area = data.api_quest_name,
-                Enemy = data.api_enemy_info.api_deck_name,
-                Rank = data.api_win_rank.First(),
-                Fight = FightID,
-                Drop= data.api_get_ship?.api_ship_name
-            };
+            //record = new ShipLog()
+            //{
+            //    Time = DateTime.Now,
+            //    Area = data.api_quest_name,
+            //    Enemy = data.api_enemy_info.api_deck_name,
+            //    Rank = data.api_win_rank.First(),
+            //    Fight = FightID,
+            //    Drop= data.api_get_ship?.api_ship_name
+            //};
 
             // 写记录
-            if (IsOnline)
-            {
-                db.ShipLog.InsertOnSubmit(record);
-                db.SubmitChanges();
-            }
-            else
-                using (FileStream fs = new FileStream(AppDomain.CurrentDomain.BaseDirectory + "\\ShipLog.csv", FileMode.Append))
-                using (StreamWriter writer = new StreamWriter(fs, Encoding.UTF8))
-                {
-                    writer.WriteLine("{0:yyyy-MM-dd HH:mm:ss.fff},{1},{2},{3},{4},{{{5}}}",
-                        record.Time,
-                        record.Area,
-                        record.Enemy,
-                        record.Rank,
-                        record.Drop,
-                        record.Fight);
-                }
         }
     }
 }
